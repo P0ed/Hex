fb7b5d435cbb619e2f2a96584d9811d28c08f27f
Konstantin Sukharev <poed@me.com>
2025-09-14 23:42:01 +0200

    Update State.swift

--- Hex Shared/State.swift
+++ Hex Shared/State.swift
@@ -1,52 +1,31 @@
 import HexKit
 
 struct State {
-	var cursor: Hex
+	var cursor: Hex = .zero
 }
 
 extension State {
 
-	init() {
-		cursor = .zero
-	}
-}
-
-extension State {
-
-	mutating func moveCursor(_ direction: Int) {
-		cursor = cursor.neighbor(direction)
+	mutating func moveCursor(_ direction: DPad) {
+		cursor = switch direction {
+		case .left: cursor.neighbor(cursor.q % 2 == 0 ? 3 : 4)
+		case .right: cursor.neighbor(cursor.q % 2 == 0 ? 1 : 0)
+		case .down: cursor.neighbor(2)
+		case .up: cursor.neighbor(5)
+		}
 	}
 
 	mutating func apply(_ action: Action) {
 		switch action {
-		case .direction(.left): moveCursor(cursor.q % 2 == 0 ? 3 : 4)
-		case .direction(.right): moveCursor(cursor.q % 2 == 0 ? 1 : 0)
-		case .direction(.down): moveCursor(2)
-		case .direction(.up): moveCursor(5)
+		case .direction(let dir): moveCursor(dir)
 		default: break
 		}
 	}
 }
 
-enum DPad {
-	case left, right, down, up
-}
+enum DPad { case left, right, down, up }
+enum Target { case prev, next }
+enum Verb { case a, b, c, d }
+enum Menu { case no, yes }
 
-enum Target {
-	case prev, next
-}
-
-enum Verb {
-	case a, b, c, d
-}
-
-enum Menu {
-	case no, yes
-}
-
-enum Action {
-	case direction(DPad)
-	case target(Target)
-	case verb(Verb)
-	case menu(Menu)
-}
+enum Action { case direction(DPad), target(Target), verb(Verb), menu(Menu) }
